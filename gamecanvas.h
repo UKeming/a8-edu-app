32d83893d8b61799ba728fc90bdb8a951298bfcd
szw1622 <u1313034@utah.edu>
2022-12-08 19:36:10 -0700

    Fix reset map bug

--- gamecanvas.h
+++ gamecanvas.h
@@ -1,3 +1,15 @@
+/**
+ * @file mainwindow.h
+ * @author Joshua Beatty, Zhuowen Song, Keming Chen, Matthew Whitaker
+ * @brief Header file for mainwindow.cpp.
+ * @version 0.1
+ * @date 2022-11-15
+ * @reviewer Joshua Beatty
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
 #ifndef GAMECANVAS_H
 #define GAMECANVAS_H
 
@@ -16,7 +28,9 @@ public:
     explicit GameCanvas(QWidget* parent, std::vector<std::vector<MapTile>> map);
 
 private:
+    // a simulation to control the robot moving
     Simulation *s;
+
     // varibles for drawing the map
     QPixmap scaledWallMap;
     QPixmap scaledBlockMap;
@@ -44,11 +58,21 @@ private:
 
     // current map
     std::vector<std::vector<MapTile>> map;
+    std::vector<std::vector<MapTile>> resetMap;
 
+    // size of map blocks
     int brickSize;
 
 public slots:
+    /**
+     * @brief setMap Draw the map on the canvas
+     * @param map
+     */
     void setMap(std::vector<std::vector<MapTile>> map);
+    /**
+     * @brief simulate Create a simulation by using the user's program and current map
+     * @param program
+     */
     void simulate(std::vector<ProgramBlock> program);
     void step();
     void run(int interval);
